0ae41d44ee888ddeb720568594f48496c1dbd73b
Karan Mishra <karanmishra2537@gmail.com>
2024-04-05 21:38:13 +0530

    learned about Handling Get Request

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -1,16 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server side!');
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   // res.status(200).send('Hello from the server side!');
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('you can post to this endpoint....');
+// app.post('/', (req, res) => {
+//   res.send('you can post to this endpoint....');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// v1 is version of api
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    results: tours.length,
+    status: 'success',
+    data: {
+      tours
+    }
+  })
 });
 
 const port = 3000;
